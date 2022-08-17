457ff5827434ee63a93b32443782388fff36b124
valq7711 <valq7711@gmail.com>
2022-08-18 00:42:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 class Foo:
     def __init__(self):
         pass
-        
+
+    def public(self):
+        """This is public"""
+        pass    
+    
     def __private__(self):
         pass    
